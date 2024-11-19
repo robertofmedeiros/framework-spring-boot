1f9594c10b088465c66850d6523a66a20e17eacf
robertofmedeiros <40504677+robertofmedeiros@users.noreply.github.com>
2024-11-19 20:25:51 -0300

    primeiro commit

---
+++ src/main/java/br/com/framework/spring/framework/FrameworkSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.framework.spring.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworkSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/framework/spring/framework/FrameworkSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.framework.spring.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
